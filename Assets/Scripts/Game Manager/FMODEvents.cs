80899e08636d64ad41be22e6701db61d8f92b582
yuri3330 <yuri3330@hotmail.ca>
2023-10-12 21:25:09 -0400

    Added Shotgun Sounds into Audio Event Manager in Unity
    Added both shotgun with a cock and no cock SFX

--- Assets/Scripts/Game Manager/FMODEvents.cs
+++ Assets/Scripts/Game Manager/FMODEvents.cs
@@ -7,6 +7,9 @@ public class FMODEvents : MonoBehaviour
 {
     [field: Header("Pistol Shot SFX")]
     [field: SerializeField] public EventReference pistolShot { get; private set; }
+    [field: Header("Shotgun Shot SFX")]
+    [field: SerializeField] public EventReference shotgunShotNoCock { get; private set; }
+    [field: SerializeField] public EventReference shotgunShotCock { get; private set; }
 
     public static FMODEvents instance {  get; private set; }
 
