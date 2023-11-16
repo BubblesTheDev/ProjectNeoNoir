26fdd5945428eee16a72d87b996615ade60ec78c
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2023-11-16 10:11:50 -0500

    Removed some unneeded files

--- Assets/Scripts/Game Manager/followPos.cs
+++ Assets/Scripts/Game Manager/followPos.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class followPos : MonoBehaviour
 {
     [SerializeField] private GameObject objToFollow;
-    [SerializeField] private bool xAxis = true;
-    [SerializeField] private bool yAxis = true;
-    [SerializeField] private bool zAxis = true;
+    [SerializeField] private bool xAxis = true, yAxis = true, zAxis = true;
+    [SerializeField] private bool smoothFollow;
+    [SerializeField] private float smoothness;
+    private Vector3 velocity;
 
-
-    private void Update()
+    private void FixedUpdate()
     {
-        if (xAxis) gameObject.transform.position = new Vector3(objToFollow.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-        if (yAxis) gameObject.transform.position = new Vector3(gameObject.transform.position.x, objToFollow.transform.position.y, gameObject.transform.position.z);
-        if (zAxis) gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, objToFollow.transform.position.z);
-;    }
+        if (!smoothFollow)
+        {
+            if (xAxis) gameObject.transform.position = new Vector3(objToFollow.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+            if (yAxis) gameObject.transform.position = new Vector3(gameObject.transform.position.x, objToFollow.transform.position.y, gameObject.transform.position.z);
+            if (zAxis) gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, objToFollow.transform.position.z);
+        }
+        else
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, objToFollow.transform.position, ref velocity, smoothness);
+        }
+    }
 }
