e463ed72fa846c412ce6efcd7280915a70a6afe9
yuri3330 <yuri3330@hotmail.ca>
2023-11-18 23:19:55 -0500

    Created event instances so that audio and or sound fx can be played through an event.

--- Assets/Scripts/Game Manager/AudioManager.cs
+++ Assets/Scripts/Game Manager/AudioManager.cs
@@ -5,21 +5,45 @@ using FMODUnity;
 
 public class AudioManager : MonoBehaviour
 {
+    private List<EventInstance> eventInstances;
+
     public static AudioManager instance { get; private set; }
 
     private void Awake()
     {
         if (instance != null)
         {
-            Debug.Log("More than  one audio manager in scene");
+            Debug.LogError("Found more than one Audio Manager in the scene");
         }
         instance = this;
-    }
 
+        eventInstances = new List<EventInstance>();
+    }
 
     public void PlaySFX(EventReference SFX, Vector3 worldPos)
     {
         RuntimeManager.PlayOneShot(SFX, worldPos);
     } 
 
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        eventInstances eventInstance = RunTimeManager.CreateInstance(eventReference);
+        evenetInstances.Add(eventInstance);
+        return eventInstance;
+    }
+
+    private void CleanUp()
+    {
+        //stop and release any created instances
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
+    }
 }
