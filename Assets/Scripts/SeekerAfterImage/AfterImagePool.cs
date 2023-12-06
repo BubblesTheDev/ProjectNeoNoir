9ed423da29536b0933764772c42d6e6ba283b403
TimothySherDev <80216575+TimothySherDev@users.noreply.github.com>
2023-12-06 16:39:56 -0500

    Seeker After Image Testing, Animation Controllers Added

---
+++ Assets/Scripts/SeekerAfterImage/AfterImagePool.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AfterImagePool : MonoBehaviour
+{
+    //  public SeekerLad mySeeker;
+    public GameObject Seeker;
+    public GameObject prefab;
+    public int poolSize = 10;
+    public List<SeekerAfterImage> afterImages;
+    public int interval = 10;
+    public int time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        afterImages = new List<SeekerAfterImage>(poolSize);
+        for (int i = 0; i < poolSize; i++) {
+            GameObject nextAfterImage = Instantiate(prefab);
+            afterImages.Add(nextAfterImage.GetComponent<SeekerAfterImage>());
+         // nextAfterImage.GetComponent<SeekerAfterImage>().targetObject = Seeker.character;
+        //  nextAfterImage.GetComponent<SeekerAfterImage>().targetAnimator = Seeker.animator;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time++;
+        if (time > interval) { time = 0; AddafterImages(); }
+
+
+    }
+
+    void AddafterImages() {
+        for(int i = 0; i < poolSize; i++)
+        if (!afterImages[i].active){ afterImages[i].Activate(); break; }
+    }
+        
+       
+}
